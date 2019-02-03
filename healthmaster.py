148e326cf7c31d9b00eac3262c2e3003edea6455
ascourtas <aristana.scourtas@corista.com>
2019-02-03 11:40:49 -0500

    add functionality to extract relevant sheet from workbook

--- healthmaster.py
+++ healthmaster.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup
-import numpy as np
 import os
 import pandas as pd
 import re
 import urllib
 import zipfile
 
+"""
+Dependencies:
+    xlrd
+    
+
+"""
 
 
-def fetch_data_files(dest_dir, url, pattern):
+def fetch_zip_files(dest_dir, url, pattern):
     """
     Fetches and writes data files (.zip files, specifically) from links scraped from the url,
     given a designated link pattern to search for.
@@ -24,7 +29,6 @@ def fetch_data_files(dest_dir, url, pattern):
     html_soup = BeautifulSoup(html_page, "html.parser")
 
     # match all links in the HTML dump that contain the designated regex pattern; these should be the zip links
-
     for link_html in html_soup.findAll('a', attrs={'href': re.compile(pattern)}):
         link = link_html.get('href')
 
@@ -48,32 +52,55 @@ def load_data(dir):
     """
     Load all files in the designated directory (dir) into a dict mapped by filename.
 
-    NOTE: currently only works with Excel files.
+    NOTE: currently only works with Excel files (.xlsx and .xls only).
 
     :param dir: Directory to load data files from.
     :return: Dict with filenames (sans extensions) as keys and pd.Dataframes of data as values.
     """
     data_dict = {}
     for filename in os.listdir(dir):
+        # TODO: remove, just meant for debugging
+        if "MGH" not in filename : continue
+
         # get name and file extension
         filename_parts = filename.split(".")
 
-        if filename_parts[1] in ['xlsx', 'xls', 'xlsm']:
+        if filename_parts[1] in ['xlsx', 'xls']:
             filepath = os.path.join(dir, filename)
             data = pd.ExcelFile(filepath)
             print "Successfully loaded {}".format(filepath)
         else:
             print "Error: File format {} not currently supported, cannot load {}".format(filename_parts[1], filepath)
-
+        # map dataframe to filename (sans extension)
         data_dict[filename_parts[0]] = data
 
     return data_dict
+
+
+def get_charge_sheet(wrkbk, sheet_index):
+    """
+    Return the designated workbook sheet from the workbook, dropping garbage rows from the Partners workbooks.
+
+    NOTE: currently only works for the Partners workbooks
+    :param wrkbk: The pd.Dataframe of the whole workbook
+    :return: The properly formatted charge sheet (pd.Dataframe) of the workbook
+    """
+    # Note: pass None as sheet_names arg to get all sheets
+    sheet = wrkbk.parse(sheet_index)
+
+    # reorganize sheet to have proper column names and drop unnecessary rows
+    sheet.columns = sheet.iloc[2]
+    # these rows are just free text info describing the chargemaster; drop them and reset the index
+    sheet = sheet.drop([0, 1, 2]).reset_index(drop=True)
+
+    return sheet
+
 
 def main():
     page_url = "https://www.partners.org/for-patients/Patient-Billing-Financial-Assistance/Hospital-Charge-Listing.aspx"
 
     # create destination directory if necessary
-    # TODO: make this configurable from CLI
+    # TODO: make this configurable
     dest_dir = "./output"
     try:
         os.makedir(dest_dir)
@@ -82,16 +109,15 @@ def main():
         pass
 
     # get data files and store them in dest_dir
-    fetch_data_files(dest_dir, page_url, "Hospital-Charges")
+    fetch_zip_files(dest_dir, page_url, "Hospital-Charges")
 
-    # create a dict of data bases, with filenames as keys (own function)
+    # create a dict of workbook Dataframes, with filenames as keys
     hospital_prices = load_data(dest_dir)
 
-    mgh =  hospital_prices['MGH Standard Charge File']
+    # get the first sheet of the MGH data
+    mgh_sheet = get_charge_sheet(hospital_prices['MGH Standard Charge File'], 0)
 
-
-
-
+    # do stuff with sheet
 
 
 if __name__ == "__main__":
